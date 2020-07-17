9cd19d24af2280d81d9f57e2b591fa6b418fd93c
Ashley Davis <ashley@codecapers.com.au>
2020-07-17 17:23:35 +1000

    First pass at resolving relationships.

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,20 @@ export async function miniql(query: any, root: any, context: any): Promise<any> 
     const output: any = {};
 
     for (const key of Object.keys(query)) {
-        output[key] = await root[key](query[key], context);
+        const resolver = root[key];
+        const subQuery = query[key];
+        output[key] = await resolver(subQuery, context);
+        if (subQuery.relate) {
+            for (const relateKey of Object.keys(subQuery.relate)) {
+                const relateId = output[key][relateKey];
+                const relateQuery: any = {};
+                relateQuery[relateKey] = {
+                    id: relateId,
+                };
+                const relateResult = await miniql(relateQuery, root, context);
+                output[key][relateKey] = relateResult[relateKey];
+            }
+        }
     }
 
     return output;
---
+++ src/test/nested-entities.test.ts
@@ -0,0 +1,52 @@
+import { miniql } from "..";
+
+describe("nested entities", () => {
+
+    it("can get one nested entity", async ()  => {
+
+        const query = {
+            movie: {
+                id: "1234",
+                relate: {
+                    director: true,
+                },
+            },
+        };
+
+        const root = {
+            movie: async (query: any, context: any) => {
+                expect(query.id).toBe("1234");
+
+                return {
+                    id: "1234",
+                    name: "Minority Report",
+                    year: 2002,
+                    director: "5678",
+                };
+            },
+
+            director: async (query: any, context: any) => {
+                expect(query.id).toBe("5678");
+
+                return {
+                    id: "5678",
+                    name: "Steven Spielberg",
+                };
+            },
+        };
+
+        const result = await miniql(query, root, {});
+        expect(result).toEqual({
+            movie: {
+                id: "1234",
+                name: "Minority Report",
+                year: 2002,
+                director: {
+                    id: "5678",
+                    name: "Steven Spielberg",
+                },
+            },
+        });
+    });
+
+});
